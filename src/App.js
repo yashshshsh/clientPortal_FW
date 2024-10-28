595b8ae47636febe1d148cd100616b0f0bb01e80
Yash <yash.sharma2484@gmail.com>
2024-10-28 11:05:40 +0530

    demo

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import './App.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import ActionTrack from './Components/ActionTrack';
+import AuditReport from './Components/AuditReport';
 
 function App() {
   return (
     <>
     <div className="App">
-      <ActionTrack/>
+     <AuditReport/>
     </div>
     </>
   );
