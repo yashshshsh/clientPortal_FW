1d2fc1742d7c40dbfba95a89d8ad50c7f15a3ff3
Yash <yash.sharma2484@gmail.com>
2024-10-30 16:02:19 +0530

    up trends

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import StoreBrowser from './Components/StoreBrowser';
-import Trends from './Components/Trends';
 
 function App() {
   return (
