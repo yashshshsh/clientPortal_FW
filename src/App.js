3b461dafa0c1bfca97228e69f045b330ca605964
Yash <yash.sharma2484@gmail.com>
2024-11-11 10:37:54 +0530

    login added

--- src/App.js
+++ src/App.js
@@ -2,42 +2,46 @@ import './App.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './Components/Navbar'
-import Dashboard from './Components/Dashboard'
-import ReportBrowser from './Components/ReportBrowser'
-import AuditReport from './Components/AuditReport'
-import ActionTrack from './Components/ActionTrack'
+import Navbar from './Components/Navbar';
+import Dashboard from './Components/Dashboard';
+import ReportBrowser from './Components/ReportBrowser';
+import AuditReport from './Components/AuditReport';
+import ActionTrack from './Components/ActionTrack';
 import UpAudits from './Components/UpAudits';
 import StoreBrowserIn from './Components/StoreBrowserIn';
 import StoreBrowser from './Components/StoreBrowser';
-import StorePerformance from './Components/StorePerf'
-import Login from './Components/Login'
-
-import AI from './Components/AI'
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import StorePerformance from './Components/StorePerf';
+import Login from './Components/Login';
+import AI from './Components/AI';
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 
 function App() {
   return (
+    <Router>
+      <AppWithRouter />
+    </Router>
+  );
+}
+
+function AppWithRouter() {
+  const location = useLocation();
+  const isNavbarVisible = location.pathname !== "/";
+
+  return (
     <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route exact path='/' element={<Login/>}></Route>
-          <Route exact path='/dashboard' element={<Dashboard />}></Route>
-          <Route exact path='/reportBrowser' element={<ReportBrowser />}></Route>
-          <Route exact path='/auditReport' element={<AuditReport />}></Route>
-          <Route exact path='/actionTrack' element={<ActionTrack />}></Route>
-          <Route exact path='/upAudits' element={<UpAudits />}></Route>
-          <Route exact path='/storeBrowser' element={<StoreBrowser/>}></Route>
-          <Route exact path='/storeBrowserInsights' element={<StoreBrowserIn/>}></Route>
-          <Route exact path='/storePerformance' element={<StorePerformance />}></Route>
-          <Route exact path='/AI' element={<AI />}></Route>
-        </Routes>
-      </Router>
+      {isNavbarVisible && <Navbar />}
+      <Routes>
+        <Route exact path='/' element={<Login />} />
+        <Route exact path="/dashboard" element={<Dashboard />} />
+        <Route exact path="/reportBrowser" element={<ReportBrowser />} />
+        <Route exact path="/auditReport" element={<AuditReport />} />
+        <Route exact path="/actionTrack" element={<ActionTrack />} />
+        <Route exact path="/upAudits" element={<UpAudits />} />
+        <Route exact path="/storeBrowser" element={<StoreBrowser />} />
+        <Route exact path="/storeBrowserInsights" element={<StoreBrowserIn />} />
+        <Route exact path="/storePerformance" element={<StorePerformance />} />
+        <Route exact path="/AI" element={<AI />} />
+      </Routes>
     </>
   );
 }
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,80 +1,79 @@
-import React from 'react'
-import logo from '../Images/Floorwalk logo7x.png'
-import leftImg from '../Images/Rectangle 1789.png'
-import rightImg from '../Images/Rectangle 1790.png'
-import { TbEyeCancel } from "react-icons/tb";
-import styles from '../CSS/Login.module.css';
-import { FiX } from 'react-icons/fi';
+import React from 'react';
+import logo from '../Images/Floorwalk logo7x.png';
+import leftImg from '../Images/Rectangle 1789.png';
+import eyeLock from '../Images/visibility_lock.png'
+import captcha from '../Images/Frame.png'
+import rightImg from '../Images/Rectangle 1790.png';
+import '../CSS/Login.css'; 
 
 const Login = () => {
   return (
-    <div>
-      <nav class={`${styles.header} navbar navbar-expand-lg`}>
-        <div class="container-fluid">
-          <div className={`${styles.logo}`}>
-            <img src={logo} alt='img'></img>
+    <div className='hero-main'>
+      <nav className="header navbar navbar-expand-lg">
+        <div className="container-fluid d-flex">
+          <div className="logo">
+            <img src={logo} alt="Logo" />
           </div>
         </div>
       </nav>
 
-      <div className="hero-section container-fluid d-flex p-0 justify-content-between">
-        <div className={`${styles.modalBody} ${styles.leftImg} d-none d-sm-inline-block`}>
-          <img src={leftImg} alt="Left" className="img-fluid" />
+      <div className="main row">
+        <div className="leftImg p-0 col-sm-2 col-md-3">
+          <img src={leftImg} alt="img" className="img-fluid"/>
         </div>
 
-        <div className={`${styles.modalBody} ${styles.heroForm} container`}>
-          <div className={`${styles.heroWelcome} mt-5`}>
-            <p style={{ color: "#003C5D" }} className={styles.wText}>Welcome to</p>
-            <p style={{ color: "#8DC63F" }} className={styles.wText}>FloorWalk Client Portal</p>
-            <p style={{ color: "#9CA3AF" }} className={styles.audLog}>
-              For auditor login <span className={`${styles.audSpan} mx-1`}>Auditor login?</span>
-            </p>
-          </div>
-
-          <div className={`${styles.modalBody} ${styles.logForm} shadow-lg mx-auto mt-4 d-flex flex-column align-items-center`}>
-            <div className={styles.logUpper}>
-              <p><span>Login</span> to your FloorWalk Client Portal</p>
-            </div>
-
-            <div className={`${styles.modalBody} ${styles.logLower} d-flex flex-column`}>
-              <div className={`${styles.email} d-flex flex-column`}>
-                <label className="d-block text-start">Email Id</label>
-                <input type="text" className={styles.inputBars} id="email" name="email" />
-              </div>
-
-              <div className={`${styles.password} my-2 d-flex flex-column`}>
-                <label className="d-block text-start">Password</label>
-                <div className={`${styles.passInput} d-flex`}>
-                  <input type="password" className={`${styles.inputBars1} w-100`} id="password" name="password" />
-                  <TbEyeCancel color="#9CA3AF" size={24} />
-                </div>
-              </div>
-
-              <div className={styles.forgot}>
-                <p className="text-start">Forgot password?</p>
-              </div>
-
-              <div className={`${styles.robot} mx-auto my-3`}>
-                <div className={`${styles.robotCheck} gap-2 mx-2 d-flex my-2`}>
-                  <input type="checkbox" id="checkbox" name="checkbox" />
-                  <p className="my-2">I'm not a robot</p>
-                </div>
-              </div>
-
-              <div className={`${styles.logInBtn} text-end mx-auto`}>
-                <button type="button" className={styles.logBtn}>Log In</button>
-              </div>
+        <div className="mainDiv col-sm-8 col-md-6">
+          <div className="welcome mx-auto mt-5 d-flex flex-column align-items-center">
+            <p className='wel'>Welcome to</p>
+            <p className='flp'>FloorWalk Client Portal</p>
+            <div className="audit-login">
+              <p>For auditor login <span className='ms-1'>Auditor login ?</span></p>
             </div>
           </div>
+
+          <div style={{ backgroundColor: "#FCFCFC" }} className="login-div mx-auto mt-3 shadow-lg p-5">
+            <div>
+              <div className="inner text-center">
+                <p><span>Login</span> to your FloorWalk Client Portal</p>
+              </div>
+              <div className="details mt-4">
+                <div class="my-3 lab-inp">
+                  <label class="form-label">Email Id</label>
+                  <input type="email" class="form-control" id="exampleInputEmail1" />
+                </div>
+                <div class="my-3 lab-inp">
+                  <label class="form-label">Password</label>
+                  <div className='df'>
+                    <input type="password" class="form-control" id="exampleInputPassword1" />
+                    <img src={eyeLock} alt='img'></img>
+                  </div>
+                </div>
+                <p className='forPass'>Forgot Password ?</p>
+                <div className="robot-div p-2 d-flex shadow-sm align-items-center justify-content-between">
+                  <div className="check df">
+                    <input type='checkbox'></input>
+                    <p className='my-2 ms-2'>I’m not a robot</p>
+                  </div>
+                  <div className="captcha">
+                    <img src={captcha} alt="img" />
+                  </div>
+                </div>
+
+                <div className="login-btn my-4 df ms-auto">
+                  <p className='my-2'>Log In</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
         </div>
 
-        <div className={`${styles.modalBody} ${styles.rightImg} d-none d-sm-inline-block`}>
-          <img src={rightImg} alt="Right" className="img-fluid" />
+        <div className="rightImg col-sm-2 col-md-3">
+          <img src={rightImg} alt="img" className="img-fluid"/>
         </div>
       </div>
     </div>
   );
 }
 
-export default Login
-
+export default Login;
